eca1893084205da05e59e1be2cfd72afc84d0ab4
olliebrownlow <olliebrownlow@gmail.com>
2019-09-28 14:44:29 +0100

    passes tests for words with leading consonants

--- app/models/terminology.rb
+++ app/models/terminology.rb
@@ -1,5 +1,12 @@
 class Terminology < ApplicationRecord
+
+  VOWELS = ["a", "e", "i", "o", "u"]
+
   def translate(terminology)
-    terminology.delete(terminology[0]).concat(terminology[0], "ay")
+    first_vowel_index = terminology.index(/[aeiou]/)
+    leading_consonants = terminology[0...first_vowel_index]
+    following_letters = terminology[(first_vowel_index)..(terminology.length - 1)]
+
+    following_letters.insert(-1, leading_consonants + "ay")
   end
 end
--- spec/models/terminology_spec.rb
+++ spec/models/terminology_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe Terminology, type: :model do
           expect(terminology.translate("risk")).to eq("iskray")
         end
       end
+
+      context 'and the word starts with a cluster of consonants' do
+        it 'returns words with a 2-consonant cluster at the end followed by "ay"' do
+          expect(terminology.translate("statement")).to eq("atementstay")
+        end
+
+        it 'returns words with a 3-consonant cluster at the end followed by "ay"' do
+          expect(terminology.translate("strap")).to eq("apstray")
+        end
+
+        it 'returns words with a 7-consonant cluster at the end followed by "ay"' do
+          expect(terminology.translate("strychnine")).to eq("inestrychnay")
+        end
+      end
     end
   end
 end
