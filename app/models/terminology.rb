b8ea807a57f695d3e48c2ee5eb685af918513651
olliebrownlow <olliebrownlow@gmail.com>
2019-09-28 14:05:00 +0100

    passes first test for model, tweaks .gitignore

--- app/models/terminology.rb
+++ app/models/terminology.rb
@@ -1,2 +1,5 @@
 class Terminology < ApplicationRecord
+  def translate(terminology)
+    terminology.delete(terminology[0]).concat(terminology[0], "ay")
+  end
 end
--- spec/models/terminology_spec.rb
+++ spec/models/terminology_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Terminology, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:terminology) { described_class.new }
+
+  describe '#translate' do
+    context 'when passed one word' do
+      context 'and the word starts with one consonant' do
+        it 'returns the word with the consonant at the end followed by "ay"' do
+          expect(terminology.translate("risk")).to eq("iskray")
+        end
+      end
+    end
+  end
 end
