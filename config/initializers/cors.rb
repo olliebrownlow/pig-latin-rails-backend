918c4539e3ca76c5849cd824f90d84644b977371
olliebrownlow <olliebrownlow@gmail.com>
2019-12-08 21:13:54 +0000

    adjusts cors.rb file to allow development locally and deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,5 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins "http://localhost:3001"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
     origins 'https://piglatin-dictionary.herokuapp.com'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
